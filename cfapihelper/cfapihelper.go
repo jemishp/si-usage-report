5db68b4ece2d32749d8bbdca1dce9c7bb5665924
Jemish Patel <jpatel@pivotal.io>
2019-05-08 10:54:24 -0700

    add getting services, service-plans & service-instances
    - service instances might suffice but we are getting more than we need
    to start with
    
    Authored-by: Jemish Patel <jpatel@pivotal.io>

--- cfapihelper/cfapihelper.go
+++ cfapihelper/cfapihelper.go
@@ -9,6 +9,9 @@ import (
 
 type CFAPIHelper interface {
 	GetOrgs() ([]plugin_models.GetOrgs_Model, error)
+	GetServices() ([]plugin_models.GetServices_Model, error)
+	GetServicePlans() ([]plugin_models.GetService_ServicePlan, error)
+	GetServiceInstances() ([]plugin_models.GetSpace_ServiceInstance, error)
 }
 
 type APIHelper struct {
@@ -40,13 +43,91 @@ func (a *APIHelper) GetOrgs() ([]plugin_models.GetOrgs_Model, error) {
 					Guid: metadata["guid"].(string),
 					Name: entity["name"].(string),
 				})
-				//Organization{
-				//	Name:      entity["name"].(string),
-				//	URL:       metadata["url"].(string),
-				//	QuotaURL:  entity["quota_definition_url"].(string),
-				//	SpacesURL: entity["spaces_url"].(string),
-				//})
 		}
 	}
 	return orgs, nil
+}
+
+func (a *APIHelper) GetServices() ([]plugin_models.GetServices_Model, error) {
+	servicesJSON, err := cfcurl.Curl(a.cliConnection, "/v2/services")
+
+	if err != nil {
+		return nil, err
+	}
+	pages := int(servicesJSON["total_pages"].(float64))
+	var services []plugin_models.GetServices_Model
+	for i := 1; i <= pages; i++ {
+		if i != 1 {
+			servicesJSON, err = cfcurl.Curl(a.cliConnection, "/v2/services?page="+strconv.Itoa(i))
+		}
+		for _, o := range servicesJSON["resources"].([]interface{}) {
+			theService := o.(map[string]interface{})
+			entity := theService["entity"].(map[string]interface{})
+			metadata := theService["metadata"].(map[string]interface{})
+			services = append(services,
+				plugin_models.GetServices_Model{
+					Guid:             metadata["guid"].(string),
+					Name:             entity["label"].(string),
+					ServicePlan:      plugin_models.GetServices_ServicePlan{},
+					Service:          plugin_models.GetServices_ServiceFields{},
+					LastOperation:    plugin_models.GetServices_LastOperation{},
+					ApplicationNames: nil,
+					IsUserProvided:   false,
+				})
+		}
+
+	}
+	return services, nil
+}
+
+func (a *APIHelper) GetServicePlans() ([]plugin_models.GetService_ServicePlan, error) {
+	servicePlansJSON, err := cfcurl.Curl(a.cliConnection, "/v2/service_plans")
+	if err != nil {
+		return nil, err
+	}
+	pages := int(servicePlansJSON["total_pages"].(float64))
+	var servicePlans []plugin_models.GetService_ServicePlan
+	for i := 1; i <= pages; i++ {
+		if i != 1 {
+			servicePlansJSON, err = cfcurl.Curl(a.cliConnection, "/v2/service_plans?page="+strconv.Itoa(i))
+		}
+		for _, o := range servicePlansJSON["resources"].([]interface{}) {
+			theServicePlan := o.(map[string]interface{})
+			entity := theServicePlan["entity"].(map[string]interface{})
+			metadata := theServicePlan["metadata"].(map[string]interface{})
+			servicePlans = append(servicePlans,
+				plugin_models.GetService_ServicePlan{
+					Name: entity["name"].(string),
+					Guid: metadata["guid"].(string),
+				})
+		}
+
+	}
+	return servicePlans, nil
+}
+
+func (a *APIHelper) GetServiceInstances() ([]plugin_models.GetSpace_ServiceInstance, error) {
+	serviceInstancesJSON, err := cfcurl.Curl(a.cliConnection, "/v2/services_instances")
+	if err != nil {
+		return nil, err
+	}
+	pages := int(serviceInstancesJSON["total_pages"].(float64))
+	var serviceInstances []plugin_models.GetSpace_ServiceInstance
+	for i := 1; i <= pages; i++ {
+		if i != 1 {
+			serviceInstancesJSON, err = cfcurl.Curl(a.cliConnection, "/v2/service_instances?page="+strconv.Itoa(i))
+		}
+		for _, o := range serviceInstancesJSON["resources"].([]interface{}) {
+			theServiceInstance := o.(map[string]interface{})
+			entity := theServiceInstance["entity"].(map[string]interface{})
+			metadata := theServiceInstance["metadata"].(map[string]interface{})
+			serviceInstances = append(serviceInstances,
+				plugin_models.GetSpace_ServiceInstance{
+					Name: entity["name"].(string),
+					Guid: metadata["guid"].(string),
+				})
+		}
+
+	}
+	return serviceInstances, nil
 }
--- cfapihelper/cfapihelper_test.go
+++ cfapihelper/cfapihelper_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 var _ = Describe("SiUsageReport", func() {
-	Describe("Setup", func() {
+	Describe("Happy path", func() {
 		var (
 			apiHelper            cfapihelper.CFAPIHelper
 			fakeClientConnection *pluginfakes.FakeCliConnection
@@ -35,6 +35,56 @@ var _ = Describe("SiUsageReport", func() {
 				Expect(len(orgs)).To(Equal(2))
 			})
 		})
+		When("26 services exist", func() {
+			var servicesJSON []string
+
+			BeforeEach(func() {
+				servicesJSON = getResponse("test-data/services.json")
+				fakeClientConnection.CliCommandWithoutTerminalOutputReturns(servicesJSON, nil)
+			})
+			It("should return list of services", func() {
+				services, err := apiHelper.GetServices()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(len(services)).To(Equal(26))
+			})
+		})
+		When("100 service plans exist", func() {
+			var servicePlansJSON []string
+
+			BeforeEach(func() {
+				servicePlansJSON = getResponse("test-data/service-plans.json")
+				fakeClientConnection.CliCommandWithoutTerminalOutputReturns(servicePlansJSON, nil)
+			})
+			It("should return list of service plans", func() {
+				services, err := apiHelper.GetServicePlans()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(len(services)).To(Equal(100))
+			})
+		})
+		When("2 service instances exist", func() {
+			var serviceInstancesJSON []string
+
+			BeforeEach(func() {
+				serviceInstancesJSON = getResponse("test-data/service-instances.json")
+				fakeClientConnection.CliCommandWithoutTerminalOutputReturns(serviceInstancesJSON, nil)
+			})
+			It("should return list of service instances", func() {
+				serviceInstances, err := apiHelper.GetServiceInstances()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(len(serviceInstances)).To(Equal(2))
+			})
+		})
+	})
+	Describe("testing error cases", func() {
+		var (
+			apiHelper            cfapihelper.CFAPIHelper
+			fakeClientConnection *pluginfakes.FakeCliConnection
+		)
+
+		BeforeEach(func() {
+			fakeClientConnection = &pluginfakes.FakeCliConnection{}
+			apiHelper = cfapihelper.New(fakeClientConnection)
+		})
 		When("no orgs exist", func() {
 			var orgsJSON []string
 
@@ -48,7 +98,47 @@ var _ = Describe("SiUsageReport", func() {
 				Expect(len(orgs)).To(Equal(0))
 			})
 		})
+		When("no services exist", func() {
+			var servicesJSON []string
+
+			BeforeEach(func() {
+				servicesJSON = getResponse("test-data/no-services.json")
+				fakeClientConnection.CliCommandWithoutTerminalOutputReturns(servicesJSON, nil)
+			})
+			It("should return an error", func() {
+				services, err := apiHelper.GetServices()
+				Expect(err).To(MatchError("CF API returned no output"))
+				Expect(len(services)).To(Equal(0))
+			})
+		})
+		When("no service plans exist", func() {
+			var servicePlansJSON []string
+
+			BeforeEach(func() {
+				servicePlansJSON = getResponse("test-data/no-service-plans.json")
+				fakeClientConnection.CliCommandWithoutTerminalOutputReturns(servicePlansJSON, nil)
+			})
+			It("should return an error", func() {
+				services, err := apiHelper.GetServicePlans()
+				Expect(err).To(MatchError("CF API returned no output"))
+				Expect(len(services)).To(Equal(0))
+			})
+		})
+		When("no service instances exist", func() {
+			var serviceInstancesJSON []string
+
+			BeforeEach(func() {
+				serviceInstancesJSON = getResponse("test-data/no-service-instances.json")
+				fakeClientConnection.CliCommandWithoutTerminalOutputReturns(serviceInstancesJSON, nil)
+			})
+			It("should return an error", func() {
+				serviceInstances, err := apiHelper.GetServiceInstances()
+				Expect(err).To(MatchError("CF API returned no output"))
+				Expect(len(serviceInstances)).To(Equal(0))
+			})
+		})
 	})
+
 })
 
 func getResponse(filename string) []string {
